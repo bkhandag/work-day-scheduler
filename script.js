023d8ba6f934e01884456b6ae610d356c023f8e8
bkhandag <khandagale.b.g@gmail.com>
2022-03-24 19:45:37 -0700

    using current time to compare timeblocks and set state

--- script.js
+++ script.js
@@ -1,2 +1,46 @@
-moment().add(3, 'days').calendar();       // Wednesday at 7:52 PM
-moment().format("MMM Do YY");              
+var currentTime = moment().format("dddd, MMMM Do, YYYY");
+$("#currentDay").text(currentTime);
+
+//Converting current time to hours.
+var currentTimeInHours = moment().hours();
+//console.log(currentTimeInHours);
+
+$(".description").each( function() {
+
+    var currentBlock = $(this).attr("id");
+    // console.log(currentBlock);
+    if(currentBlock == currentTimeInHours){
+
+       $(this).addClass("present");
+
+    } else if(currentBlock < currentTimeInHours) {
+
+        $(this).addClass("past");
+
+    } else { 
+        
+        $(this).addClass("future")
+    }   
+
+});
+
+
+
+//1.Initialize p # current day with current time using moment.js
+//2. Initialize function for the page that populates this Time
+//3. Build out time blocks in html given the existing styling
+//4. write an event handler for current time.
+//5. Write an event listener that initiates comparision of time blocks with current time
+//6. Write Function to compare timeblocks:
+//6.1 If timeblock == past time, color it grey.
+//6..2 if timeblock == current time, color it red
+// 6.3 if timeblock == future time, color it green.
+// 6.4 May be include this in the init as well
+// 7 Write a func to set local storage by allowing user to add description
+// 7.1 Identify html tag to use to allow user entry
+// 7.2 .....
+// 7.3 Save string to local storage upon click of button.
+// 8. Write a function to get item to to keep value populated on screen.
+// 8.1 ...
+// 9 Tie both get/set items to init function.
+//10. SEt a timeout to update time every 5 minutes and update class
