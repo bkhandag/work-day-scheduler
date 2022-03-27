242ce50eae148241a9ca0516954c53987f8f7ad1
bkhandag <khandagale.b.g@gmail.com>
2022-03-26 18:38:21 -0700

    Added get item using each function, added comments and readme

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
 # work-day-scheduler
+
+## User Story
+
+```md
+AS AN employee with a busy schedule
+I WANT to add important events to a daily planner
+SO THAT I can manage my time effectively
+```
+
+## Acceptance Criteria
+
+```md
+GIVEN I am using a daily planner to create a schedule
+WHEN I open the planner
+THEN the current day is displayed at the top of the calendar
+WHEN I scroll down
+THEN I am presented with timeblocks for standard business hours
+WHEN I view the timeblocks for that day
+THEN each timeblock is color coded to indicate whether it is in the past, present, or future
+WHEN I click into a timeblock
+THEN I can enter an event
+WHEN I click the save button for that timeblock
+THEN the text for that event is saved in local storage
+WHEN I refresh the page
+THEN the saved events persist
+```
+
+## Link to Application:
+https://bkhandag.github.io/work-day-scheduler/
+
+## Pseudo Code:
+1. Initialize p # current day with current time using moment.js
+1. Initialize function for the page that populates this Time
+1. Build out time blocks in html given the existing styling
+1. write an event handler for current time.
+1. Write an event listener that initiates comparision of time blocks with current time
+1. Write Function to compare timeblocks:
+1. If timeblock == past time, color it grey.
+1. if timeblock == current time, color it red
+1. if timeblock == future time, color it green.
+1. May be include this in the init as well
+1. Write a func to set local storage by allowing user to add description
+1. Identify html tag to use to allow user entry
+1. Save string to local storage upon click of button.
+1. Write a function to get item to to keep value populated on screen.
+
+## Screenshot
+!["Work Day Scheduler".](work_day_scheduler.png)
+
--- script.js
+++ script.js
@@ -1,20 +1,6 @@
+//Defined global variables
 var eventText;
-
-$(".saveBtn").on('click', function(event) {
-
-    eventText = $(".description").val();
-    console.log(eventText);
-    localStorage.setItem(".textarea",eventText);
-
-});
-
-$(window).on('load', function(event) { 
-
-    console.log("You refreeshed!")
-    eventText = localStorage.getItem("textarea");
-    $(".textarea").val(eventText);
-
-});
+var currentEventID;
 
 //Getting current time using moment()
 var currentTime = moment().format("dddd, MMMM Do, YYYY");
@@ -23,19 +9,22 @@ $("#currentDay").text(currentTime);
 //Converting current time to hours.
 var currentTimeInHours = moment().hours();
 
-//Compare current time in hours with id for each block to set the styling
+//Compare current time in hours with id for each time block to set the styling
 $(".time-block").each( function() {
 
     var currentBlock = $(this).attr("id");
-    // console.log(currentBlock);
-    if(currentBlock == currentTimeInHours){
+
+    //If the current block id = time in hours
+    if(currentBlock == currentTimeInHours-5){
 
        $(this).addClass("present");
 
-    } else if(currentBlock < currentTimeInHours) {
+       //If the current block id is less than current time in hours
+    } else if(currentBlock < currentTimeInHours-5) {
 
         $(this).addClass("past");
 
+        //If neither first conditions are true then set class to future
     } else { 
         
         $(this).addClass("future")
@@ -43,24 +32,35 @@ $(".time-block").each( function() {
 
 });
 
+//Event handler for clicking of save button after entering an event to be stored
+$(".saveBtn").on('click', function(event) {
+    
+    //Store in variable the event is entered by the user
+    eventText = $(event.currentTarget).siblings('.textarea').val();
+
+    //Get the current event Id for the textarea which is a sibling of button pressed by user.
+    currentEventID = $(event.currentTarget).siblings('.textarea').attr("id");
+
+    //Store string (value) entered by user for the ID (key);
+    localStorage.setItem(currentEventID, eventText);
+
+});
+
+//Function to execute upon loading of page
+$(window).on('load', function(event) { 
+
+    //Loop through textarea, for any id that has a value, getitem for each.
+    $(".time-block").each( function() {
+
+        //Get all event IDs for this class
+        var currentEventID = $(this).attr("id");
+
+        //Get value for the specified key from local storage
+        var eventText = localStorage.getItem(currentEventID);
+        //Apply the value to each ID
+        $(this).text(eventText);
+    })
+
+});
 
 
-
-//1.Initialize p # current day with current time using moment.js
-//2. Initialize function for the page that populates this Time
-//3. Build out time blocks in html given the existing styling
-//4. write an event handler for current time.
-//5. Write an event listener that initiates comparision of time blocks with current time
-//6. Write Function to compare timeblocks:
-//6.1 If timeblock == past time, color it grey.
-//6..2 if timeblock == current time, color it red
-// 6.3 if timeblock == future time, color it green.
-// 6.4 May be include this in the init as well
-// 7 Write a func to set local storage by allowing user to add description
-// 7.1 Identify html tag to use to allow user entry
-// 7.2 .....
-// 7.3 Save string to local storage upon click of button.
-// 8. Write a function to get item to to keep value populated on screen.
-// 8.1 ...
-// 9 Tie both get/set items to init function.
-//10. SEt a timeout to update time every 5 minutes and update class
